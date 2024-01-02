21ad3f1bb8f7075b5cf111da99559be3e5a0a044
Tentastisch <studiotent22@gmail.com>
2024-01-02 21:39:40 +0100

    added: Class for HttpClient errors

---
+++ TenorSharp/Models/HttpError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenorSharp.Models
+{
+    internal class HttpError
+    {
+        public error error { get; set; }
+    }
+
+    internal class error
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+}
