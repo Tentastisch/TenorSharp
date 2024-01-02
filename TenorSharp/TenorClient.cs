45a38dde1796f234103d9c1ddfa5927e3ec7afd9
Tentastisch <studiotent22@gmail.com>
2024-01-02 21:42:04 +0100

    function: Search method and logger support

--- TenorSharp/TenorClient.cs
+++ TenorSharp/TenorClient.cs
@@ -1,6 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using TenorSharp.Parameters;
+using Microsoft.Extensions.Logging;
 using TenorSharp.Models;
-using TenorSharp.Parameters;
+using System.Text.Json;
 
 namespace TenorSharp
 {
@@ -9,7 +10,8 @@ namespace TenorSharp
         private string _key;
         private string? _clientKey;
 
-        
+        public ILogger logger;
+
         public TenorClient(string apiKey)
         {
             if (string.IsNullOrEmpty(apiKey))
@@ -18,6 +20,17 @@ namespace TenorSharp
             this._key = apiKey;
         }
 
+        public TenorClient(string apiKey, string clientKey)
+        {
+            if (string.IsNullOrEmpty(apiKey))
+                throw new ArgumentNullException(nameof(apiKey));
+
+            if (string.IsNullOrEmpty(clientKey))
+                throw new ArgumentNullException(nameof(clientKey));
+
+            this._key = apiKey;
+            this._clientKey = clientKey;
+        }
         public int Ping()
         {
             var apiUrl = new LinkBuilder(ApiModes.autocomplete, _key);
@@ -26,27 +39,78 @@ namespace TenorSharp
             return 0;
         }
 
-        public void Search(SearchParameter searchParameter)
+        public SearchResults Search(string query, int amount = 20)
+        {
+            var searchParameter = new SearchParameter();
+            searchParameter.query = query;
+            searchParameter.limit = amount;
+
+            return Search(searchParameter);
+        }
+
+        public SearchResults Search(SearchParameter searchParameter)
         {
             var linkBuilder = new LinkBuilder(ApiModes.search, _key);
             linkBuilder.AddParameters(searchParameter.Build(_key));
             var apiUrl = linkBuilder.Build();
 
-            Console.WriteLine(apiUrl);
+            using (var httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(apiUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var respondJson = response.Content.ReadAsStringAsync().Result;
+                    var searchResult = JsonSerializer.Deserialize<SearchResults>(respondJson);
+
+                    return searchResult;
+                }
+                else
+                {
+                    var errorContent = response.Content.ReadAsStringAsync().Result;
+                    HttpError error = JsonSerializer.Deserialize<HttpError>(errorContent);
+                    logger.LogError("Error {Code} - {Message}", error.error.code, error.error.message);
+                }
+            }
+
+            return null;
+        }
+
+        public async Task<SearchResults> SearchAsync(string query, int amount = 20)
+        {
+            var searchParameter = new SearchParameter();
+            searchParameter.query = query;
+            searchParameter.limit = amount;
+
+            return await SearchAsync(searchParameter);
+        }
+
+        public async Task<SearchResults> SearchAsync(SearchParameter searchParameter)
+        {
+            var linkBuilder = new LinkBuilder(ApiModes.search, _key);
+            linkBuilder.AddParameters(searchParameter.Build(_key));
+            var apiUrl = linkBuilder.Build();
 
             using (var httpClient = new HttpClient())
             {
-                var json = httpClient.GetStringAsync(apiUrl).Result;
+                var response = await httpClient.GetAsync(apiUrl);
 
-                Console.WriteLine(json);
+                if (response.IsSuccessStatusCode)
+                {
+                    var respondJson = await response.Content.ReadAsStringAsync();
+                    var searchResult = JsonSerializer.Deserialize<SearchResults>(respondJson);
 
-                //SearchResult results = JsonConvert.DeserializeObject<SearchResult>(json);
+                    return searchResult;
+                }
+                else
+                {
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    HttpError error = JsonSerializer.Deserialize<HttpError>(errorContent);
+                    logger.LogError("Error {Code} - {Message}", error.error.code, error.error.message);
+                }
             }
-        }
 
-        public async Task SearchAsync(SearchParameter searchParameter)
-        {
-
+            return null;
         }
     }
 }
