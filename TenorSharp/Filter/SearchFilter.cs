16004def5b028e19c30311e25d548aaccd831d56
Tentastisch <studiotent22@gmail.com>
2024-01-02 18:04:18 +0100

    created: Search filter

--- TenorSharp/Filter/SearchFilter.cs
+++ TenorSharp/Filter/SearchFilter.cs
@@ -6,8 +6,48 @@ using System.Threading.Tasks;
 
 namespace TenorSharp.Filter
 {
-    public enum SearchFilter
+    public class SearchFilter
     {
+        private bool sticker = false;
+        private bool stat = false;
+        private bool minStat = false;
 
+        private SearchFilter(bool sticker, bool stat, bool minStat)
+        {
+            this.sticker = sticker;
+            this.stat = stat;
+            this.minStat = minStat;
+        }
+
+        public SearchFilter Sticker()
+        {
+            return new SearchFilter(true, false, false);
+        }
+
+        public SearchFilter OnlyAnimated()
+        {
+            return new SearchFilter(true, false, true);
+        }
+
+        public SearchFilter OnlyStatic()
+        {
+            return new SearchFilter(true, true, false);
+        }
+
+        public string ToString()
+        {
+            StringBuilder builder = new StringBuilder("");
+            
+            if (sticker)
+                builder.Append("sticker");
+
+            if (stat)
+                builder.Append(",static");
+
+            if (minStat)
+                builder.Append(",-static");
+
+            return builder.ToString();
+        }
     }
 }
