08d0c56c6e8139328480920a32ea1b50d4e7e236
Tentastisch <studiotent22@gmail.com>
2024-01-03 12:47:52 +0100

    refactor: changed media formats

--- TenorSharp/Models/MediaFormats.cs
+++ TenorSharp/Models/MediaFormats.cs
@@ -8,14 +8,23 @@ namespace TenorSharp.Models
 {
     public class MediaFormats
     {
-        public MediaFormat nanogifpreview { get; set; }
-        public MediaFormat nanomp4 { get; set; }
-        public MediaFormat tinymp4 { get; set; }
-        public MediaFormat gifpreview { get; set; }
+        public MediaFormat preview { get; set; }
         public MediaFormat gif { get; set; }
-        public MediaFormat mp4 { get; set; }
-        public MediaFormat tinygifpreview { get; set; }
-        public MediaFormat nanogif { get; set; }
+        public MediaFormat mediumgif { get; set; }
         public MediaFormat tinygif { get; set; }
+        public MediaFormat nanogif { get; set; }
+        public MediaFormat mp4 { get; set; }
+        public MediaFormat loopedmp4 { get; set; }
+        public MediaFormat tinymp4 { get; set; }
+        public MediaFormat nanomp4 { get; set; }
+        public MediaFormat webm { get; set; }
+        public MediaFormat tinywebm { get; set; }
+        public MediaFormat nanowebm { get; set; }
+        public MediaFormat webp_transparent { get; set; }
+        public MediaFormat tinywebp_transparent { get; set; }
+        public MediaFormat nanowebp_transparent { get; set; }
+        public MediaFormat gif_transparent { get; set; }
+        public MediaFormat tinygif_transparent { get; set; }
+        public MediaFormat nanogif_transparent { get; set; }
     }
 }
