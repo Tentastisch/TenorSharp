de9deeebd634c9090f411c238e8fbabec3269c92
Tentastisch <studiotent22@gmail.com>
2024-01-03 12:49:35 +0100

    fix + docs: Changed ToString visibility to internal

--- TenorSharp/Filter/SearchFilter.cs
+++ TenorSharp/Filter/SearchFilter.cs
@@ -6,12 +6,21 @@ using System.Threading.Tasks;
 
 namespace TenorSharp.Filter
 {
+    /// <summary>
+    /// Filter to receive animated and static sticker. Do not use this on GIFs.
+    /// </summary>
     public class SearchFilter
     {
         private bool sticker = false;
         private bool stat = false;
         private bool minStat = false;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sticker"></param>
+        /// <param name="stat"></param>
+        /// <param name="minStat"></param>
         private SearchFilter(bool sticker, bool stat, bool minStat)
         {
             this.sticker = sticker;
@@ -19,22 +28,38 @@ namespace TenorSharp.Filter
             this.minStat = minStat;
         }
 
+        /// <summary>
+        /// Creates a filter to receive static and animated sticker content.
+        /// </summary>
+        /// <returns>Filter for static and animated sticker</returns>
         public SearchFilter Sticker()
         {
             return new SearchFilter(true, false, false);
         }
 
+        /// <summary>
+        /// Creates a filter to receive animated sticker content only
+        /// </summary>
+        /// <returns>Filter for animated sticker</returns>
         public SearchFilter OnlyAnimated()
         {
             return new SearchFilter(true, false, true);
         }
 
+        /// <summary>
+        /// Creates a filter to receive static sticker content only.
+        /// </summary>
+        /// <returns>Filter for static sticker</returns>
         public SearchFilter OnlyStatic()
         {
             return new SearchFilter(true, true, false);
         }
 
-        public string ToString()
+        /// <summary>
+        /// Internal method to make filter into a string.
+        /// </summary>
+        /// <returns></returns>
+        internal string ToString()
         {
             StringBuilder builder = new StringBuilder("");
             
